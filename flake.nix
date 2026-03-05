01781fcf3d633e6d6df8426170a154296b0d8938
Travis Cole <11240+kelp@users.noreply.github.com>
2026-03-05 08:20:55 -0800

    Release v0.7.2

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
       in {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "vibeutils";
-          version = "0.7.1";
+          version = "0.7.2";
 
           src = ./.;
 
